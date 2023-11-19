2890ff41d98c160407a454dd5041c03b9475e2d7
Eduardo Bueno <eduardo@ebssolucoescorporativas.com.br>
2023-11-19 17:56:16 -0400

    add aula51 a aula54

---
+++ aula54.py
@@ -0,0 +1,7 @@
+"""
+Faça uma lista de comprar com lista
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores de sua lista
+Não permita que o programa quebra com 
+erros de índices inexistentes na lista
+"""
