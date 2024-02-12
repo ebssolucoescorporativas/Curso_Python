3d19c5ebdee0e0f2be83c76010ee1a6836269a01
Eduardo Bueno <eduardo@ebssolucoescorporativas.com.br>
2024-02-12 15:59:38 -0400

    add aula93 to aula100

---
+++ aula100.package/modulo.py
@@ -0,0 +1,15 @@
+def aumenta_10_por_cento(lista):
+
+    produtos = [
+        {'nome': 'Produto 5', 'preco': 10.00},
+        {'nome': 'Produto 1', 'preco': 22.32},
+        {'nome': 'Produto 3', 'preco': 10.11},
+        {'nome': 'Produto 2', 'preco': 105.87},
+        {'nome': 'Produto 4', 'preco': 69.90},
+    ]
+
+    return [
+         {**produto, 'preco': produto['preco'] * 1.10}
+        for produto in produtos
+    ]
+
---
+++ aula97_m.py
@@ -0,0 +1,8 @@
+# print('Este módulo se chama', __name__)
+
+print('Este módulo se chama', __name__)
+variavel_modulo = 'Luiz'
+
+
+def soma(x, y):
+    return x + y
