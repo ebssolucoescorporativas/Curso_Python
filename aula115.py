3d4ea1ff4cba760e0c11a473fda0af4f9ec93b9f
Eduardo Bueno <eduardo@ebssolucoescorporativas.com.br>
2024-02-15 17:46:29 -0400

    add aula107 to aula 115

---
+++ aula115.py
@@ -0,0 +1,27 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+#
+#
+# Set-ExecutionPolicy AllSigned -Force --> para liberar o uso do ambinte virtual
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+#
+# Ativando e desativando meu ambiente virtual
+# Windows: .\venv\Scripts\activate
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
+#
